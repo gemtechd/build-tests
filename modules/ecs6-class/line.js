15f90cbef9ca5185c9870b6d8ca8f21896575d10
yl6343 <yl6751914@gmail.com>
2024-07-09 14:52:37 +0300

    pull from base

--- modules/ecs6-class/line.js
+++ modules/ecs6-class/line.js
@@ -2,6 +2,8 @@ const Point = require("./point");
 
 class Line {
     constructor({ point1 = new Point(), point2 = new Point(), n = undefined, slope = undefined }) {
+        if ((typeof (n) !== 'number' && typeof (n) !== 'undefined') || (typeof (slope) !== 'number' && typeof (slope) !== 'undefined'))
+            throw Error('n or slope are not a number')
         this.point1 = point1;
         this.point2 = point2;
         this.slope = slope;
@@ -9,7 +11,11 @@ class Line {
     }
 
     calculateSlope = () => {
-        this.slope = (this.point1.y - this.point2.y) / (this.point1.x - this.point2.x)
+        if (this.point1.x === this.point2.x)
+            throw Error('Division error by zero')
+        else {
+            this.slope = (this.point1.y - this.point2.y) / (this.point1.x - this.point2.x)
+        }
     }
 
     calculateNOfLineFunction = () => {
@@ -24,15 +30,22 @@ class Line {
         return this.getPointByX(0)
     }
 
-
     getPointByX(x) {
+        if (typeof (x) !== 'number')
+            throw Error('x is not a number')
         let y = this.slope * x + this.n
         return new Point({ x, y })
     }
 
     getPointByY(y) {
-        let x = (y - this.slope) / this.n;
-        return new Point({ x, y })
+        if (typeof (y) !== 'number')
+            throw Error('y is not a number')
+        if (this.slope === 0)
+            throw Error('Division error by zero')
+        else {
+            let x = (y - this.n) / this.slope;
+            return new Point({ x, y })
+        }
     }
 }
 
