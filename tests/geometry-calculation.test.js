d0980e6f975e8c2afa524aac09f06c3214e91e44
Faigy <nagel0863@gmail.com>
2024-07-28 16:25:17 +0300

    add test

---
+++ tests/geometry-calculation.test.js
@@ -0,0 +1,120 @@
+const geometry = require('../modules/geometry-calculation')
+const Point = require('../modules/ecs6-class/point')
+const Line = require('../modules/ecs6-class/line')
+
+const point1 = new Point({ x: 1, y: 1 })
+const point2 = new Point({ x: 2, y: 2 })
+const line1 = new Line({ point1, point2, n: 2, slope: 2 })
+
+
+
+describe('CALCULATE_DISTANCE', () => {
+    test('', () => {
+        expect(geometry.calculateDistance(point1, point2)).toBe(1.4142135623730951)
+    });
+});
+
+describe('ERRORS', () => {
+    test('check errors of calculateDistance', () => {
+        expect(() => geometry.calculateDistance({ point1: 'c' }, { point1: [1, 2] })).toThrow('the function should get two arguments of "Point"')
+        expect(() => geometry.calculateDistance({ point1: [1, 2] }, { point1: true })).toThrow('the function should get two arguments of "Point"')
+        expect(() => geometry.calculateDistance({ point1: true }, { point1: (f) => f })).toThrow('the function should get two arguments of "Point"')
+        expect(() => geometry.calculateDistance({ point1: (f) => f }, { point1: () => f })).toThrow('the function should get two arguments of "Point"')
+        expect(() => geometry.calculateDistance({ point1: () => f }, { point1: 'c' })).toThrow('the function should get two arguments of "Point"')
+        expect(() => geometry.calculateDistance({ point1: 'c' })).toThrow('the function should get two arguments of "Point"')
+        expect(() => geometry.calculateDistance({ point1: [1, 2] })).toThrow('the function should get two arguments of "Point"')
+        expect(() => geometry.calculateDistance({ point1: true })).toThrow('the function should get two arguments of "Point"')
+        expect(() => geometry.calculateDistance({ point1: (f) => f })).toThrow('the function should get two arguments of "Point"')
+        expect(() => geometry.calculateDistance({ point1: () => f })).toThrow('the function should get two arguments of "Point"')
+    });
+});
+
+describe('CALCULATE_JUNCTION_POINT', () => {
+    test('check if line1.slope end n equal to line 2', () => {
+        const line2 = new Line({ point1, point2, n: 2, slope: 2 })
+        expect(geometry.calculateJunctionPoint(line1, line2)).toBe(true)
+    });
+    test('', () => {
+        const line2 = new Line({ point1, point2, n: 3, slope: 2 })
+        expect(geometry.calculateJunctionPoint(line1, line2)).toBe(false)
+    });
+    test('', () => {
+        const line2 = new Line({ point1, point2, n: 2, slope: 6 })
+        expect(geometry.calculateJunctionPoint(line1, line2)).toEqual({ x: 0, y: 2 })
+    });
+    test('mock on calculateJunctionPoint', () => {
+        const line2 = new Line({ n: 2, slope: 2 });
+        jest.spyOn(line1, 'getPointByX').mockImplementation((x) => {
+            const y = line1.slope * x + line1.n;
+            return new Point({ x, y });
+        });
+
+        jest.spyOn(line2, 'getPointByX').mockImplementation((x) => {
+            const y = line2.slope * x + line2.n;
+            return new Point({ x, y });
+        });
+        const result = geometry.calculateJunctionPoint(line1, line2);
+        expect(result).toBe(true)
+
+    })
+});
+
+describe('ERRORS', () => {
+    test('check errors of calculateJunctionPoint', () => {
+        expect(() => geometry.calculateJunctionPoint({ line1: 'c' }, { line2: [1, 2] })).toThrow('the function should get two arguments of "Line"')
+        expect(() => geometry.calculateJunctionPoint({ line1: [1, 2] }, { line2: true })).toThrow('the function should get two arguments of "Line"')
+        expect(() => geometry.calculateJunctionPoint({ line1: true }, { line2: (l) => l })).toThrow('the function should get two arguments of "Line"')
+        expect(() => geometry.calculateJunctionPoint({ line1: (f) => f }, { line2: () => l })).toThrow('the function should get two arguments of "Line"')
+        expect(() => geometry.calculateJunctionPoint({ line1: () => f }, { line2: 'jh' })).toThrow('the function should get two arguments of "Line"')
+        expect(() => geometry.calculateJunctionPoint({ line1: () => f })).toThrow('the function should get two arguments of "Line"')
+        expect(() => geometry.calculateJunctionPoint({ line1: 'c' })).toThrow('the function should get two arguments of "Line"')
+        expect(() => geometry.calculateJunctionPoint({ line1: [1, 2] })).toThrow('the function should get two arguments of "Line"')
+        expect(() => geometry.calculateJunctionPoint({ line1: true })).toThrow('the function should get two arguments of "Line"')
+        expect(() => geometry.calculateJunctionPoint({ line1: (f) => f })).toThrow('the function should get two arguments of "Line"')
+
+    });
+});
+
+describe('IS_POINT_ON LINE', () => {
+    test('should return true if line1.slope end n equal to line 2', () => {
+        const point = new Point({ x: 3, y: 3 })
+        const line2 = new Line({ point1: line1.point1, point })
+        line2.calculateSlope()
+        line2.calculateNOfLineFunction()
+        expect(geometry.isPointOnLine(line2, point)).toBe(true)
+    });
+
+    test('should return false if line1.slope not equal to line2', () => {
+        const point = new Point({ x: 5, y: 3 })
+        const line2 = new Line({ point1: line1.point1, point })
+        line2.calculateSlope()
+        line2.calculateNOfLineFunction()
+        expect(geometry.isPointOnLine(line2, point)).toBe(false)
+    });
+
+    test('should return false if line1.n not equal to line2', () => {
+        const point = new Point({ x: 6, y: 6 })
+        const line2 = new Line({ point1, point2, n: 5 })
+        line2.calculateSlope()
+        expect(geometry.isPointOnLine(line2, point)).toBe(false)
+    });
+    test('mock on isPointOnLine', () => {
+
+    });
+
+    describe('ERRORS', () => {
+        test('check errors of isPointOnLine', () => {
+            expect(() => geometry.isPointOnLine({ point: 'c' }, { line: [1, 2] })).toThrow('the function should get arg of "Point" and arg of "Line"')
+            expect(() => geometry.isPointOnLine({ point: [1, 2] }, { line: (f) => f })).toThrow('the function should get arg of "Point" and arg of "Line"')
+            expect(() => geometry.isPointOnLine({ point: (f) => f }, { line: true })).toThrow('the function should get arg of "Point" and arg of "Line"')
+            expect(() => geometry.isPointOnLine({ point: true }, { line: () => f })).toThrow('the function should get arg of "Point" and arg of "Line"')
+            expect(() => geometry.isPointOnLine({ point: () => f }, { line: 'c' })).toThrow('the function should get arg of "Point" and arg of "Line"')
+            expect(() => geometry.isPointOnLine({ line: 'c' })).toThrow('the function should get arg of "Point" and arg of "Line"')
+            expect(() => geometry.isPointOnLine({ line: [1, 2] })).toThrow('the function should get arg of "Point" and arg of "Line"')
+            expect(() => geometry.isPointOnLine({ line: true })).toThrow('the function should get arg of "Point" and arg of "Line"')
+            expect(() => geometry.isPointOnLine({ line: (f) => f })).toThrow('the function should get arg of "Point" and arg of "Line"')
+            expect(() => geometry.isPointOnLine({ line: () => f })).toThrow('the function should get arg of "Point" and arg of "Line"')
+        });
+    });
+});
+
