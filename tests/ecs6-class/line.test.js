eb7394bde44ad0e45741321fb0287c456e413c04
M-Grun <nrho374@gmail.com>
2024-07-28 16:53:44 +0300

    add test

--- tests/ecs6-class/line.test.js
+++ tests/ecs6-class/line.test.js
@@ -2,62 +2,66 @@ const Line = require('../../modules/ecs6-class/line');
 const Point = require('../../modules/ecs6-class/point');
 const mockGetPointByY = jest.fn(() => ({ x: 0, y: 0 }));
 const mockGetPointByX = jest.fn(() => ({ x: 0, y: 5 }));
-const point1 = new Point({ x: 4, y: 3 });
-const point2 = new Point({ x: 3, y: 2 });
-const line = new Line({ point1, point2 });
-const line1 = new Line({});
-line1.slope = 0;
-const line2 = new Line({ point1, point2 });
-line1.n = 0;
-const line3 = new Line({});
-line3.slope = 2;
-const line4 = new Line({ point1, point2 });
-line4.n = 0;
 
-describe('ERRORS', () => {
-    test('should throw string error when argument are no point', () => {
-        expect(() => new Line({ point1: new Line({}) })).toThrow('point1 not valid')
-        expect(() => new Line({ point2: 'hello' })).toThrow('point2 not valid')
-        expect(() => new Line({ point1: ['x', 'y'] })).toThrow('point1 not valid')
-        expect(() => new Line({ point2: false })).toThrow('point2 not valid')
-        expect(() => new Line({ point1: () => true, point2: () => 'y' })).toThrow('point1 and point2 not valid')
+describe('CONSTRUCTOR', () => {
+    it('should build line when get point', () => {
+        const line = new Line({ point1: new Point({ x: 2, y: 2 }) })
+        expect(line.point1).toEqual(new Point({ x: 2, y: 2 }));
     })
 
-    test('should throw string error when argumonts are no number string', () => {
-        expect(() => new Line({ slope: 'a' })).toThrow('slope should be a number')
-        expect(() => new Line({ slope: true })).toThrow('slope should be a number')
-        expect(() => new Line({ slope: ['a', 'b'] })).toThrow('slope should be a number')
-        expect(() => new Line({ slope: value => (value) })).toThrow('slope should be a number')
-    })
+    describe('ERRORS', () => {
+        it('should throw string error when argument are no point', () => {
+            expect(() => new Line({ point1: new Line({}) })).toThrow('point1 not valid')
+            expect(() => new Line({ point2: 'hello' })).toThrow('point2 not valid')
+            expect(() => new Line({ point1: ['x', 'y'] })).toThrow('point1 not valid')
+            expect(() => new Line({ point2: false })).toThrow('point2 not valid')
+            expect(() => new Line({ point1: () => true, point2: () => 'y' })).toThrow('point1 and point2 not valid')
+        })
 
-    test('should throw string error when argumonts are no number string', () => {
-        expect(() => new Line({ n: 'a' })).toThrow('n should be a number')
-        expect(() => new Line({ n: true })).toThrow('n should be a number')
-        expect(() => new Line({ n: ['a', 'b'] })).toThrow('n should be a number')
-        expect(() => new Line({ n: value => (value) })).toThrow('n should be a number')
+        it('should throw string error when argumonts are no number string', () => {
+            expect(() => new Line({ slope: 'a' })).toThrow('slope should be a number')
+            expect(() => new Line({ slope: true })).toThrow('slope should be a number')
+            expect(() => new Line({ slope: ['a', 'b'] })).toThrow('slope should be a number')
+            expect(() => new Line({ slope: value => (value) })).toThrow('slope should be a number')
+        })
+
+        it('should throw string error when argumonts are no number string', () => {
+            expect(() => new Line({ n: 'a' })).toThrow('n should be a number')
+            expect(() => new Line({ n: true })).toThrow('n should be a number')
+            expect(() => new Line({ n: ['a', 'b'] })).toThrow('n should be a number')
+            expect(() => new Line({ n: value => (value) })).toThrow('n should be a number')
+        })
     })
 })
 
 describe('CALCULATE_SLOPE', () => {
-    test('should calculate the slope', () => {
+    it('should calculate the slope', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point2 = new Point({ x: 3, y: 2 });
+        const line = new Line({ point1, point2 });
         line.calculateSlope();
         expect(line.slope).toBe(1)
     })
 
-    test('should throw string error when point1.x - point2.x equal 0', () => {
+    it('should throw string error when point1.x - point2.x equal 0', () => {
+        const line1 = new Line({});
         expect(() => line1.calculateSlope()).toThrow('can`t divide by 0')
     })
 })
 
 describe('CALCULATE_N_OF_LINE_FUNCTION', () => {
-    test('should calculate n of line', () => {
+    it('should calculate n of line', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point2 = new Point({ x: 3, y: 2 });
+        const line = new Line({ point1, point2 });
+        line.slope = 1;
         line.calculateNOfLineFunction()
         expect(line.n).toBe(-1)
     })
 })
 
 describe('GET_POINT_ON_X_ASIS', () => {
-    test('mocking getPointOnXAsis', () => {
+    it('mock getPointOnXAsis', () => {
         const LineInstance = new Line({});
         LineInstance.getPointByY = mockGetPointByY;
         LineInstance.getPointOnXAsis();
@@ -66,28 +70,40 @@ describe('GET_POINT_ON_X_ASIS', () => {
 })
 
 describe('GET_POINT_ON_Y_ASIS', () => {
-    test('mocking getPointOnYAsis', () => {
+    it('mock getPointOnYAsis', () => {
         const LineInstance = new Line(2, 3);
         LineInstance.getPointByX = mockGetPointByX;
-        LineInstance.getPointOnYAsis();
+        const result = LineInstance.getPointOnYAsis();
         expect(mockGetPointByX).toHaveBeenCalledWith(0);
+        expect(result).toEqual({ x: 0, y: 5 });
     });
 })
 
 describe('GET_POINT_BY_X', () => {
-    test('should return point x', () => {
+    it('should return point x', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point2 = new Point({ x: 3, y: 2 });
+        const line = new Line({ point1, point2 });
         expect(line.getPointByX(5)).toEqual({ x: 5, y: 4 })
     })
 
-    test('should to send to function: calculateSlope if slope is undefined', () => {
+    it('should to send to function: calculateSlope if slope is undefined', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point2 = new Point({ x: 3, y: 2 });
+        const line2 = new Line({ point1, point2 });
         expect(line2.getPointByX(5)).toEqual({ x: 5, y: 4 })
     })
 
-    test('should to send to function: calculateNOfLineFunction if n is undefined', () => {
+    it('should to send to function: calculateNOfLineFunction if n is undefined', () => {
+        const line1 = new Line({});
+        line1.slope = 0;
         expect(line1.getPointByX(5)).toEqual({ x: 5, y: 0 })
     })
 
-    test('should throw string error when argumonts are no number string or undefined when argument is undefined', () => {
+    it('should throw string error when argumonts are no number string or undefined when argument is undefined', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point2 = new Point({ x: 3, y: 2 });
+        const line = new Line({ point1, point2 });
         expect(() => line.getPointByX('a')).toThrow('argument is not a number')
         expect(() => line.getPointByX(true)).toThrow('argument is not a number')
         expect(() => line.getPointByX(['x', 'y'])).toThrow('argument is not a number')
@@ -97,19 +113,33 @@ describe('GET_POINT_BY_X', () => {
 })
 
 describe('GET_POINT_BY_Y', () => {
-    test('should return point y', () => {
+    it('should return point y', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point2 = new Point({ x: 3, y: 2 });
+        const line = new Line({ point1, point2 });
         expect(line.getPointByY(2)).toEqual({ x: 3, y: 2 })
     })
 
-    test('should to send to function: calculateSlope if slope is undefined', () => {
+    it('should to send to function: calculateSlope if slope is undefined', () => {
+        const line3 = new Line({});
+        line3.slope = 2;
         expect(line3.getPointByY(2)).toEqual({ x: 1, y: 2 })
     })
 
-    test('should to send to function: calculateNOfLineFunction if n is undefined', () => {
+    it('should to send to function: calculateNOfLineFunction if n is undefined', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point2 = new Point({ x: 3, y: 2 });
+        const line4 = new Line({ point1, point2 });
+        line4.n = 0;
         expect(line4.getPointByY(2)).toEqual({ x: 2, y: 2 })
     })
 
-    test('should throw string error when argumonts are no number string or undefined when argument is undefined', () => {
+    it('should throw string error when argumonts are no number string or undefined when argument is undefined', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point2 = new Point({ x: 3, y: 2 });
+        const line = new Line({ point1, point2 });
+        const line1 = new Line({});
+        line1.slope = 0;
         expect(() => line.getPointByY('a')).toThrow('argument is not a number')
         expect(() => line.getPointByY(true)).toThrow('argument is not a number')
         expect(() => line.getPointByY(['x', 'y'])).toThrow('argument is not a number')
--- tests/geometry-calculation.test.js
+++ tests/geometry-calculation.test.js
@@ -1,28 +1,15 @@
 const Line = require('../modules/ecs6-class/line');
 const Point = require('../modules/ecs6-class/point');
 const { calculateDistance, calculateJunctionPoint, isPointOnLine } = require('../modules/geometry-calculation');
-const point1 = new Point({ x: 4, y: 3 });
-const point2 = new Point({ x: 3, y: 2 });
-const line1 = new Line({ point1, point2, slope: 2, n: 3 });
-
-const point3 = new Point({ x: 2, y: 2 });
-const point4 = new Point({ x: 2, y: 1 });
-const line2 = new Line({ point1: point3, point2: point4, slope: 2, n: 5 });
-const line3 = new Line({ point1: point3, point2: point4, slope: 4, n: 3 });
-
-const point5 = new Point({ x: 2, y: 2 });
-const point6 = new Point({ x: 1, y: 1 });
-const line4 = new Line({ point1: point5, point2: point6, slope: 1, n: 0 });
-const line5 = new Line({ point1: point5, point2: point4, slope: 1, n: 3 });
-const line6 = new Line({ point1: point5, point2: point6, n: 0 });
-const line7 = new Line({ point1: point5, point2: point4, slope: 1 });
 
 describe('CALCULATE_DISTANCE', () => {
-    test('should calculate distance', () => {
+    it('should calculate distance', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point2 = new Point({ x: 3, y: 2 });
         expect(calculateDistance(point1, point2)).toEqual(1)
     })
 
-    test('should throw string error when argumonts are no number string or undefined when argument is undefined', () => {
+    it('should throw string error when argumonts are no number string or undefined when argument is undefined', () => {
         expect(() => calculateDistance(new Line({}), new Line({}))).toThrow('the argument should be point')
         expect(() => calculateDistance(1, 2)).toThrow('the argument should be point')
         expect(() => calculateDistance(true)).toThrow('the function should get two arguments')
@@ -33,19 +20,32 @@ describe('CALCULATE_DISTANCE', () => {
 })
 
 describe('CALCULATE_JUNCTION_POINT', () => {
-    test('should return true', () => {
+    it('should return true', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point2 = new Point({ x: 3, y: 2 });
+        const line1 = new Line({ point1, point2, slope: 2, n: 3 });
         expect(calculateJunctionPoint(line1, line1)).toEqual(true)
     })
 
-    test('should return false', () => {
+    it('should return false', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point2 = new Point({ x: 3, y: 2 });
+        const line1 = new Line({ point1, point2, slope: 2, n: 3 });
+        const point3 = new Point({ x: 2, y: 2 });
+        const point4 = new Point({ x: 2, y: 1 });
+        const line2 = new Line({ point1: point3, point2: point4, slope: 2, n: 5 });
         expect(calculateJunctionPoint(line1, line2)).toEqual(false)
     })
 
-    test('should return new point', () => {
+    it('should return new point', () => {
+        const point3 = new Point({ x: 2, y: 2 });
+        const point4 = new Point({ x: 2, y: 1 });
+        const line2 = new Line({ point1: point3, point2: point4, slope: 2, n: 5 });
+        const line3 = new Line({ point1: point3, point2: point4, slope: 4, n: 3 });
         expect(calculateJunctionPoint(line2, line3)).toEqual({ x: 1, y: 7 })
     })
 
-    test('should throw string error when argumonts are no number string or undefined when argument is undefined', () => {
+    it('should throw string error when argumonts are no number string or undefined when argument is undefined', () => {
         expect(() => calculateJunctionPoint(new Point(), new Point())).toThrow('the argument should be line')
         expect(() => calculateJunctionPoint(1, 2)).toThrow('the argument should be line')
         expect(() => calculateJunctionPoint(true)).toThrow('the function should get two arguments')
@@ -56,27 +56,46 @@ describe('CALCULATE_JUNCTION_POINT', () => {
 })
 
 describe('IS_POINT_ON_LINE', () => {
-    test('should return true if the slope and n equals', () => {
+    it('should return true if the slope and n equals', () => {
+        const point5 = new Point({ x: 2, y: 2 });
+        const point6 = new Point({ x: 1, y: 1 });
+        const line4 = new Line({ point1: point5, point2: point6, slope: 1, n: 0 });
         expect(isPointOnLine(line4, point6)).toEqual(true)
     })
 
-    test('should return false if the slope equal and n not equal', () => {
+    it('should return false if the slope equal and n not equal', () => {
+        const point4 = new Point({ x: 2, y: 1 });
+        const point5 = new Point({ x: 2, y: 2 });
+        const point6 = new Point({ x: 1, y: 1 });
+        const line5 = new Line({ point1: point5, point2: point4, slope: 1, n: 3 });
         expect(isPointOnLine(line5, point6)).toEqual(false)
     })
 
-    test('should return true if the slope and n not equals', () => {
+    it('should return true if the slope and n not equals', () => {
+        const point4 = new Point({ x: 2, y: 1 });
+        const point1 = new Point({ x: 4, y: 3 });
+        const point2 = new Point({ x: 3, y: 2 });
+        const line1 = new Line({ point1, point2, slope: 2, n: 3 });
         expect(isPointOnLine(line1, point4)).toEqual(false)
     })
 
-    test('should to send to function: calculateSlope if slope is undefined', () => {
+    it('should to send to function: calculateSlope if slope is undefined', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point5 = new Point({ x: 2, y: 2 });
+        const point6 = new Point({ x: 1, y: 1 });
+        const line6 = new Line({ point1: point5, point2: point6, n: 0 });
         expect(isPointOnLine(line6, point1)).toEqual(false)
     })
 
-    test('should to send to function: calculateNOfLineFunction if n is undefined', () => {
+    it('should to send to function: calculateNOfLineFunction if n is undefined', () => {
+        const point1 = new Point({ x: 4, y: 3 });
+        const point4 = new Point({ x: 2, y: 1 });
+        const point5 = new Point({ x: 2, y: 2 });
+        const line7 = new Line({ point1: point5, point2: point4, slope: 1 });
         expect(isPointOnLine(line7, point1)).toEqual(false)
     })
 
-    test('should throw string error when argumonts are no number string or undefined when argument is undefined', () => {
+    it('should throw string error when argumonts are no number string or undefined when argument is undefined', () => {
         expect(() => isPointOnLine(new Point(), new Point())).toThrow('the argument should be line')
         expect(() => isPointOnLine(new Line({}), new Line({}))).toThrow('the argument should be point')
         expect(() => isPointOnLine(1, 2)).toThrow('the argument should be line')
