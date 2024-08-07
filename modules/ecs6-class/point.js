2864ae7b8e81964e3ad9e73d848c251d69493a3b
rachel-yos <ryos122004@gmail.com>
2024-08-07 13:18:07 +0300

    changes in modules according to tests requirements

--- modules/ecs6-class/point.js
+++ modules/ecs6-class/point.js
@@ -1,12 +1,18 @@
 class Point {
-    constructor({x=0, y=0}={}) {
+    constructor({ x = 0, y = 0 } = {}) {
         this.x = x;
         this.y = y;
     }
     moveVertical(value) {
+        if (typeof (value) !== "number") {
+            throw new Error("value is NaN")
+        }
         this.y += value;
     }
     moveHorizontal(value) {
+        if (typeof (value) !== "number") {
+            throw new Error("value is NaN")
+        }
         this.x += value;
     }
 }
